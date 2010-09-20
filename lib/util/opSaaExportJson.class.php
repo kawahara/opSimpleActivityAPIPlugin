140c59e2e5d89f1bd729ba03ae101d0ee7613950
Shogo Kawahara <kawahara@bucyou.net>
2010-09-21 03:38:53 +0900

    fixed opSaaExportJson to convert statuses data correctly

--- lib/util/opSaaExportJson.class.php
+++ lib/util/opSaaExportJson.class.php
@@ -4,7 +4,35 @@ class opSaaExportJson extends opSaaExport
 {
   public function export(array $data)
   {
-    return json_encode($data);
+    $convertData = $this->convert($data);
+
+    return json_encode($convertData);
+  }
+
+  protected function convert(array $array)
+  {
+    $result = array();
+
+    foreach ($array as $key => $data)
+    {
+      if (is_array($data))
+      {
+        if (0 === count($data) || $key === 'statuses' || $key === 'status')
+        {
+          return $this->convert($data);
+        }
+        else
+        {
+          $result[$key] = $this->convert($data);
+        }
+      }
+      else
+      {
+        $result[$key] = $data;
+      }
+    }
+
+    return $result;
   }
 
   public function getContentType()
--- test/unit/util/opSaaExportJsonTest.php
+++ test/unit/util/opSaaExportJsonTest.php
@@ -2,7 +2,14 @@
 
 include_once dirname(__FILE__).'/../../bootstrap/unit.php';
 
-$t = new lime_test(1, new lime_output_color());
+$t = new lime_test(2, new lime_output_color());
 
 $instance = new opSaaExportJson();
 $t->is($instance->export(array('foo' => array('foo' => 'bar'))), '{"foo":{"foo":"bar"}}', '->export() returns string of JSON');
+
+$t->is($instance->export(array(
+  'statuses' => array(
+    array('status' => array('foo1' => 'bar', 'foo2' => 'bar')),
+    array('status' => array('foo1' => 'bar', 'foo2' => 'bar')),
+  )
+)), '[{"foo1":"bar","foo2":"bar"},{"foo1":"bar","foo2":"bar"}]', '->export() returns string of JSON');
